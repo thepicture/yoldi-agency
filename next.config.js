4e1cfe090b38661e66393cc043ae679c1e1ac326
thepicture <85333076+thepicture@users.noreply.github.com>
2023-02-17 22:59:45 -0500

    fix(pages/*): fix lose of scss styles

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-        appDir: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
 };
 
 module.exports = nextConfig;
