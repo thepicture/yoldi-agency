9d2e5359499752fced0dcdafc6c959347e615262
thepicture <85333076+thepicture@users.noreply.github.com>
2023-02-23 19:20:03 -0500

    feat(pages/account): adapt ui

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -5,11 +5,14 @@ import styles from './Button.module.scss';
 export interface ButtonProps {
     text: string;
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+
+    icon?: React.ReactNode;
 }
 
-export const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ text, onClick, icon }) => {
     return (
         <button className={styles.button} type="button" onClick={onClick}>
+            {icon && <span className={styles.icon}>{icon}</span>}
             {text}
         </button>
     );
